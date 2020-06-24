33140d05fd16a7d4e71a2d6a84c071efab43574a
SDchao <wentaochens@outlook.com>
2020-06-24 18:45:48 +0800

    Complete add/remove event in Manager

--- src/team/webgroup/todo/Manager.java
+++ src/team/webgroup/todo/Manager.java
@@ -1,4 +1,25 @@
 package team.webgroup.todo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Manager {
+    private ArrayList<Event> eventArrayList;
+
+    public Manager(Event[] eventsToAdd) {
+        this.eventArrayList = new ArrayList<>();
+        eventArrayList.addAll(Arrays.asList(eventsToAdd));
+    }
+
+    public ArrayList<Event> getEvents() {
+        return (ArrayList<Event>) eventArrayList.clone();
+    }
+
+    public void addEvent(Event e) {
+        eventArrayList.add(e);
+    }
+
+    public void removeEvent(int index) {
+        eventArrayList.remove(index);
+    }
 }
