b3a05610ee4eb203310f4885dd9672fd25e43caf
SDchao <wentaochens@outlook.com>
2020-06-24 16:38:26 +0800

    Init Project

---
+++ src/team/webgroup/todo/Manager.java
@@ -0,0 +1,4 @@
+package team.webgroup.todo;
+
+public class Manager {
+}
---
+++ src/test/team/webgroup/todo/ManagerTest.java
@@ -0,0 +1,18 @@
+package test.team.webgroup.todo; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+public class ManagerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+
+} 
