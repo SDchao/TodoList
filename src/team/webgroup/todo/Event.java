5161eb71cad8eff0d5d93d1ddb8d38531d267d4c
notelekS <sk_ca55559@foxmail.com>
2020-06-24 17:18:36 +0800

    first commit

--- src/team/webgroup/todo/Event.java
+++ src/team/webgroup/todo/Event.java
@@ -1,4 +1,35 @@
 package team.webgroup.todo;
 
 public class Event {
+
+    private String title;
+    private String content;
+    private boolean isCompleted;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public Event() {
+    }
 }
