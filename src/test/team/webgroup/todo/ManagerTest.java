d21c4eee8510a5297b2300439514b423b885a601
SDchao <wentaochens@outlook.com>
2020-06-25 17:39:07 +0800

    Add modify event test

--- src/test/team/webgroup/todo/ManagerTest.java
+++ src/test/team/webgroup/todo/ManagerTest.java
@@ -30,32 +30,5 @@ public void testManager() {
 
     //TODO: modify event
 }
-@Test
-public void testremoveEvent(){
-    Event event1 = new Event("1st event","1st event contest",false);
-    manager.removeEvent(event1);
-
-    ArrayList<Event> events = manager.removeEvents();
-    Assert.assertNull(event1);
-}
-@Test
-public void testsetComplete(){
-    Event event1 = new Event("1st event","1st event contest",false);
-    Event event2 = new Event("1st event","1st event contest",true);
-    manager.setComplete(event1, Boolean isCompleted);
-
-    ArrayList<Event> events = manager.setComplete();
-    Assert.assertEquals(event1,event2);
-}
-@Test
-public void testmodifyEvent(){
-    Event event1 = new Event("1st event","1st event contest",false);
-    Event event2 = new Event("2nd event","2nd event contest",false);
-    manager.modifyEvent(event1,2nd event,2nd event contest);
-
-    ArrayList<Event> events = manager.modifyEvents();
-    Assert.assertEquals(event1,event2);
-}
-
 
 } 
